e9171a54d2d64e4075e666fb4542210cff1ce15b
ricardomangore <ricardomangore@gmail.com>
2015-10-09 10:20:45 -0500

    se agrego el directorio spanish para mensajes en español

---
+++ application/language/spanish/message_lang.php
@@ -0,0 +1,48 @@
+<?php
+//shared
+
+$lang['saludo'] = 'Hola bien venido al sistema';
+
+$lang['title_login_opusx'] = 'Ingrese al Sistema';
+
+//Formulario de Login
+$lang['label_user_name'] 		= 'Nombre de Usuario';
+$lang['error_required_username'] = 'El Nombre de Usuario es obligatorio';
+$lang['error_required_password'] = 'El Password es obligatorio';
+$lang['error_required_username_password'] = 'El nombre de Usuario y el Password son obligatorios';
+$lang['error_login_message'] = 'El usuario no esta registrado'; 
+$lang['error_login_auth'] = 'La cuenta de usuario no existe';
+
+//Formulario Add Región
+$lang['error_required_region'] = 'El campo Región es obligatorio';
+//Formulario Add Aerolínea
+$lang['error_required_aerolinea'] = 'El campo Aerolínea es obligatorio';
+//Formulario Delete Aerolinea
+$lang['error_foreingkey_aerolinea'] = 'La Aerolínea tienen recargos relacionados';
+//Formulario Recargo Aereo
+$lang['error_foreingkey_recargo_aereo'] = 'El Recargo Aéreo esta relacionado con Fletes Aéreos';
+//Formulario Add Aeropuerto
+$lang['error_required_aeropuerto'] = 'El campo Aeropuerto es obligatorio';
+$lang['error_required_code'] = 'El campo Code es obligatorio';
+//Formulario Add Recargo Aéreo
+$lang['error_required_clave'] = 'El campo Clave es obligatorio';
+$lang['error_required_costo'] = 'El campo Costo es obligatorio';
+
+$lang['error_aerolinea_required'] = 'Seleccione una aerolínea';
+
+//Formulario Add Puerto
+
+$lang['error_required_puerto'] = "El campo puerto es obligatorio";
+$lang['error_foreingkey_puerto'] = 'El Puerto esta relacionado con otros registros';
+
+//Formulario Add Naviera
+$lang['error_required_naviera'] = "El campo Naviera el obligatorio";
+$lang['error_foreingkey_naviera'] = 'La Naviera esta relacionado con otros registros';
+
+//Formulario Add Contenedor
+$lang['error_required_pies'] = "El campo Pies es obligatorio";
+$lang['error_required_peso'] = "El campo Peso es obligatorio";
+$lang['error_required_volumen'] = "El campo Volumen es obligatorio";
+$lang['error_required_tare'] = "El campo Tare es obligatorio";
+$lang['error_required_tipo'] = "El campo Tipo es obligatorio";
+
